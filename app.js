fcf5e5a737dbfb946cd030458a329039e1f07fc2
Hannah Vile <72998745+HanaYezi@users.noreply.github.com>
2021-11-21 21:26:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,13 +53,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   cardArray.sort(() => 0.5 - Math.random())
 
-
   const grid = document.querySelector('.grid')
   const resultDisplay = document.querySelector('#result')
   let cardsChosen = []
   let cardsChosenId = []
   let cardsWon = []
- 
 
   //create your board
   function createBoard() {
@@ -67,43 +65,41 @@ document.addEventListener('DOMContentLoaded', () => {
       const card = document.createElement('img')
       card.setAttribute('src', 'images/blank.png')
       card.setAttribute('data-id', i)
-      //card.addEventListener('click', flipCard)
+      card.addEventListener('click', flipCard)
       grid.appendChild(card)
     }
   }
 
+  //check for matches
   function checkForMatch() {
     const cards = document.querySelectorAll('img')
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
+    
     if(optionOneId == optionTwoId) {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
       alert('You have clicked the same image!')
+    }
     else if (cardsChosen[0] === cardsChosen[1]) {
-        alert('You found a match')
-        cards[optionOneId].setAttribute('src', 'images/white.png')
-        cards[optionTwoId].setAttribute('src', 'images/white.png')
-        cards[optionOneId].removeEventListener('click', flipCard)
-        cards[optionTwoId].removeEventListener('click', flipCard)
-        cardsWon.push(cardsChosen)
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'images/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
     } else {
-        cards[optionOneId].setAttribute('src', 'images/blank.png')
-        cards[optionTwoId].setAttribute('src', 'images/blank.png')
-        alert('Sorry, try again')
-      }
-     cardsChosen = []
-     cardsChosenId = []
-     resultDisplay.textContent = cardsWon.length
-     if  (cardsWon.length === cardArray.length/2) {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
       resultDisplay.textContent = 'Congratulations! You found them all!'
     }
-
-
-    }
-
-
-
+  }
 
   //flip your card
   function flipCard() {
@@ -117,3 +113,5 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   createBoard()
+})
+
